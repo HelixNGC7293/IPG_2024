bb5e4a826dee45a456da04907ed14622900b51d3
DGSpitzer3090 <wenguhu@gmail.com>
2024-04-09 05:06:42 -0400

    Add Stack Example

--- Example10_ProceduralLabyrinth/Assets/Scripts/OtherExamples/StringFormatTester.cs
+++ Example10_ProceduralLabyrinth/Assets/Scripts/OtherExamples/StringFormatTester.cs
@@ -7,8 +7,12 @@ public class StringFormatTester : MonoBehaviour
 {
     string npcName = "Eddy";
     string playerName = "Bob";
-    // Start is called before the first frame update
-    void Start()
+
+	//***Example - Stack
+	private Stack<string> history = new Stack<string>();
+
+	// Start is called before the first frame update
+	void Start()
     {
         //Use string.Format to replace strings
         string original1 = "Hello {0}, this is {1}";
@@ -17,11 +21,15 @@ public class StringFormatTester : MonoBehaviour
         //Without using string.Format, directly read variables
 		string original2 = $"Hello {npcName}, this is {playerName}";
 		print(original2);
-	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+		//***Example - Stack
+		history.Push("Page 1");
+		history.Push("Page 2");
+		history.Push("Page 3");
+		string previousPage = history.Pop();
+		Debug.Log("Going back to: " + previousPage);
+		Debug.Log("Remain Pages: " + history.Count);
+	}
 }
